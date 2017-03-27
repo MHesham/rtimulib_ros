a138a8e278158fe032aa7b74213a15b02590d1d4
MHesham <mhesham.fcis@gmail.com>
2017-03-26 18:02:35 -0700

    Make node more customizable and bug fixing
    - Reformat the code to follow ROS C++ coding convention.
    - Fix a bug in which acceleration is being sent as units of G instead of m/s^2.
    - Publish raw imu reading without fusion as well as magnetic field and euler messages.
    - Make publishing all messages optional.
    - Fallback to RTIMULib IMU recommended poll rate if update_rate param is not provided.
    - Emit ROS_FATAL and ROS_BREAK on errors.

--- src/rtimulib_ros.cpp
+++ src/rtimulib_ros.cpp
@@ -13,7 +13,8 @@
 //     names of its contributors may be used to endorse or promote products
 //     derived from this software without specific prior written permission.
 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND
 // ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 // WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
 // DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
@@ -27,96 +28,242 @@
 #include <RTIMULib.h>
 #include <ros/ros.h>
 #include <sensor_msgs/Imu.h>
+#include <sensor_msgs/MagneticField.h>
 
-int main(int argc, char **argv)
+static const double G_TO_MPSS = 9.80665;
+static const double uT_TO_T = 1000000.0;
+
+int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "rtimulib_node");
-    ROS_INFO("Imu driver is now running");
-    ros::NodeHandle n;
-    ros::NodeHandle private_n("~");
+  ros::init(argc, argv, "rtimulib_node");
+  ROS_INFO("Imu driver is now running");
+  ros::NodeHandle n;
+  ros::NodeHandle private_n("~");
+  const uint32_t queue_depth = 1;
 
-    std::string topic_name;
-    if(!private_n.getParam("topic_name", topic_name))
+  bool publish_data;
+  if (!private_n.getParam("publish_data", publish_data))
+  {
+    ROS_WARN("No publish_data provided - default: true");
+    publish_data = true;
+  }
+
+  ros::Publisher data_pub;
+  if (publish_data)
+  {
+    std::string data_topic_name;
+    if (!private_n.getParam("data_topic_name", data_topic_name))
     {
-        ROS_WARN("No topic_name provided - default: imu/data");
-        topic_name = "imu/data";
+      ROS_WARN("No data_topic_name provided - default: data");
+      data_topic_name = "data";
     }
 
-    std::string calibration_file_path;
-    if(!private_n.getParam("calibration_file_path", calibration_file_path))
+    data_pub =
+        n.advertise<sensor_msgs::Imu>(data_topic_name.c_str(), queue_depth);
+  }
+
+  bool publish_data_raw;
+  if (!private_n.getParam("publish_data_raw", publish_data_raw))
+  {
+    ROS_WARN("No publish_data provided - default: true");
+    publish_data_raw = true;
+  }
+
+  ros::Publisher data_raw_pub;
+  if (publish_data_raw)
+  {
+    std::string data_raw_topic_name;
+    if (!private_n.getParam("data_raw_topic_name", data_raw_topic_name))
     {
-        ROS_ERROR("The calibration_file_path parameter must be set to use a calibration file.");
+      ROS_WARN("No data_raw_topic_name provided - default: data_raw");
+      data_raw_topic_name = "data_raw";
     }
 
-    std::string calibration_file_name;
-    if(!private_n.getParam("calibration_file_name", calibration_file_name))
+    data_raw_pub =
+        n.advertise<sensor_msgs::Imu>(data_raw_topic_name.c_str(), queue_depth);
+  }
+
+  bool publish_mag;
+  if (!private_n.getParam("publish_mag", publish_mag))
+  {
+    ROS_WARN("No publish_mag provided - default: false");
+    publish_mag = false;
+  }
+
+  ros::Publisher mag_pub;
+  if (publish_mag)
+  {
+    std::string mag_topic_name;
+    if (!private_n.getParam("mag_topic_name", mag_topic_name))
     {
-        ROS_WARN("No calibration_file_name provided - default: RTIMULib.ini");
-        calibration_file_name = "RTIMULib";
+      ROS_WARN("No mag_topic_name provided - default: mag");
+      mag_topic_name = "mag";
     }
 
-    std::string frame_id;
-    if(!private_n.getParam("frame_id", frame_id))
+    mag_pub = n.advertise<sensor_msgs::MagneticField>(mag_topic_name.c_str(),
+                                                      queue_depth);
+  }
+
+  bool publish_euler;
+  if (!private_n.getParam("publish_euler", publish_euler))
+  {
+    ROS_WARN("No publish_euler provided - default: false");
+    publish_euler = false;
+  }
+
+  ros::Publisher euler_pub;
+  if (publish_euler)
+  {
+    std::string euler_topic_name;
+    if (!private_n.getParam("euler_topic_name", euler_topic_name))
     {
-        ROS_WARN("No frame_id provided - default: imu_link");
-        frame_id = "imu_link";
+      ROS_WARN("No euler_topic_name provided - default: euler");
+      euler_topic_name = "euler";
     }
 
-    double update_rate;
-    if(!private_n.getParam("update_rate", update_rate))
+    euler_pub = n.advertise<geometry_msgs::Vector3>(euler_topic_name.c_str(),
+                                                    queue_depth);
+  }
+
+  std::string calibration_file_path;
+  if (!private_n.getParam("calibration_file_path", calibration_file_path))
+  {
+    ROS_ERROR("The calibration_file_path parameter must be set to use a "
+              "calibration file.");
+    ROS_BREAK();
+  }
+
+  std::string calibration_file_name;
+  if (!private_n.getParam("calibration_file_name", calibration_file_name))
+  {
+    ROS_WARN("No calibration_file_name provided - default: RTIMULib.ini");
+    calibration_file_name = "RTIMULib";
+  }
+
+  std::string frame_id;
+  if (!private_n.getParam("frame_id", frame_id))
+  {
+    ROS_WARN("No frame_id provided - default: imu_link");
+    frame_id = "imu_link";
+  }
+
+  // Load the RTIMULib.ini config file
+  RTIMUSettings* settings = new RTIMUSettings(calibration_file_path.c_str(),
+                                              calibration_file_name.c_str());
+
+  RTIMU* imu = RTIMU::createIMU(settings);
+
+  if ((imu == NULL) || (imu->IMUType() == RTIMU_TYPE_NULL))
+  {
+    ROS_ERROR("No Imu found");
+    ROS_BREAK();
+  }
+
+  // Initialise the imu object
+  if (!imu->IMUInit())
+  {
+    ROS_FATAL("Failed to init the IMU");
+    ROS_BREAK();
+  }
+
+  // Set the Fusion coefficient
+  imu->setSlerpPower(0.02);
+  // Enable the sensors
+  imu->setGyroEnable(true);
+  imu->setAccelEnable(true);
+  imu->setCompassEnable(true);
+
+  double update_rate;
+  if (!private_n.getParam("update_rate", update_rate))
+  {
+    update_rate = 1000.0 / (double)imu->IMUGetPollInterval();
+    ROS_WARN(
+        "No update_rate provided - default to IMU recommended poll rate: %f Hz",
+        update_rate);
+  }
+
+  ROS_INFO("IMU Poll Rate: %f Hz", update_rate);
+
+  ros::Rate loop_rate(update_rate);
+  ros::Time current_time;
+  sensor_msgs::Imu data_msg;
+  sensor_msgs::Imu data_raw_msg;
+  sensor_msgs::MagneticField mag_msg;
+  geometry_msgs::Vector3 euler_msg;
+
+  data_msg.header.frame_id = frame_id;
+  data_raw_msg.header.frame_id = frame_id;
+  mag_msg.header.frame_id = frame_id;
+
+  while (ros::ok())
+  {
+    if (imu->IMURead())
     {
-        ROS_WARN("No update_rate provided - default: 20 Hz");
-        update_rate = 20;
+      current_time = ros::Time::now();
+
+      RTIMU_DATA imu_data = imu->getIMUData();
+
+      if (publish_data)
+      {
+        data_msg.header.stamp = current_time;
+
+        data_msg.orientation.x = imu_data.fusionQPose.x();
+        data_msg.orientation.y = imu_data.fusionQPose.y();
+        data_msg.orientation.z = imu_data.fusionQPose.z();
+        data_msg.orientation.w = imu_data.fusionQPose.scalar();
+
+        data_msg.angular_velocity.x = imu_data.gyro.x();
+        data_msg.angular_velocity.y = imu_data.gyro.y();
+        data_msg.angular_velocity.z = imu_data.gyro.z();
+
+        // Convert linear acceleration from G to m/s^2
+        data_msg.linear_acceleration.x = imu_data.accel.x() * G_TO_MPSS;
+        data_msg.linear_acceleration.y = imu_data.accel.y() * G_TO_MPSS;
+        data_msg.linear_acceleration.z = imu_data.accel.z() * G_TO_MPSS;
+
+        data_pub.publish(data_msg);
+      }
+
+      if (publish_data_raw)
+      {
+        data_raw_msg.header.stamp = current_time;
+
+        data_raw_msg.angular_velocity.x = imu_data.gyro.x();
+        data_raw_msg.angular_velocity.y = imu_data.gyro.y();
+        data_raw_msg.angular_velocity.z = imu_data.gyro.z();
+
+        // Convert linear acceleration from G to m/s^2
+        data_raw_msg.linear_acceleration.x = imu_data.accel.x() * G_TO_MPSS;
+        data_raw_msg.linear_acceleration.y = imu_data.accel.y() * G_TO_MPSS;
+        data_raw_msg.linear_acceleration.z = imu_data.accel.z() * G_TO_MPSS;
+
+        data_raw_pub.publish(data_raw_msg);
+      }
+
+      if (publish_mag && imu_data.compassValid)
+      {
+        mag_msg.header.stamp = current_time;
+
+        // Convert linear acceleration from uT to T
+        mag_msg.magnetic_field.x = imu_data.compass.x() / uT_TO_T;
+        mag_msg.magnetic_field.y = imu_data.compass.y() / uT_TO_T;
+        mag_msg.magnetic_field.z = imu_data.compass.z() / uT_TO_T;
+
+        mag_pub.publish(mag_msg);
+      }
+
+      if (publish_euler)
+      {
+        euler_msg.x = imu_data.fusionPose.x() * RTMATH_RAD_TO_DEGREE;
+        euler_msg.y = imu_data.fusionPose.y() * RTMATH_RAD_TO_DEGREE;
+        euler_msg.z = imu_data.fusionPose.z() * RTMATH_RAD_TO_DEGREE;
+        euler_pub.publish(euler_msg);
+      }
     }
 
-    ros::Publisher imu_pub = n.advertise<sensor_msgs::Imu>(topic_name.c_str(), 1);
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
 
-    // Load the RTIMULib.ini config file
-    RTIMUSettings *settings = new RTIMUSettings(calibration_file_path.c_str(), calibration_file_name.c_str()); 
-
-    RTIMU *imu = RTIMU::createIMU(settings);
-
-    if ((imu == NULL) || (imu->IMUType() == RTIMU_TYPE_NULL))
-    {
-        ROS_ERROR("No Imu found");
-        return -1;
-    }
-
-    // Initialise the imu object
-    imu->IMUInit();
-
-    // Set the Fusion coefficient
-    imu->setSlerpPower(0.02);
-    // Enable the sensors
-    imu->setGyroEnable(true);
-    imu->setAccelEnable(true);
-    imu->setCompassEnable(true);
-
-    ros::Rate loop_rate(update_rate);
-    while (ros::ok())
-    {
-        sensor_msgs::Imu imu_msg;
-
-        if (imu->IMURead())
-        {
-            RTIMU_DATA imu_data = imu->getIMUData();
-            imu_msg.header.stamp = ros::Time::now();
-            imu_msg.header.frame_id = frame_id;
-            imu_msg.orientation.x = imu_data.fusionQPose.x(); 
-            imu_msg.orientation.y = imu_data.fusionQPose.y(); 
-            imu_msg.orientation.z = imu_data.fusionQPose.z(); 
-            imu_msg.orientation.w = imu_data.fusionQPose.scalar(); 
-            imu_msg.angular_velocity.x = imu_data.gyro.x();
-            imu_msg.angular_velocity.y = imu_data.gyro.y();
-            imu_msg.angular_velocity.z = imu_data.gyro.z();
-            imu_msg.linear_acceleration.x = imu_data.accel.x();
-            imu_msg.linear_acceleration.y = imu_data.accel.y();
-            imu_msg.linear_acceleration.z = imu_data.accel.z();
-
-            imu_pub.publish(imu_msg);
-        }
-        ros::spinOnce();
-        loop_rate.sleep();
-    }
-    return 0;
+  return 0;
 }
